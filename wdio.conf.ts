b5b1c5398f7ddbe7dcca1bff35b9ff2af2073a8b
Pham Manh Tien <abc160cba@gmail.com>
2025-03-29 22:57:49 +0700

    update/automation/test/vs_3

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,32 +1,29 @@
 export const config = {
+    hostname: 'localhost',
+    port: 4723,
+    path: '/',
     runner: 'local',
+    specs: ['./test/specs/**/*.ts'],
     framework: 'mocha',
-    sync: true,
-    reporters: ['spec'],
-    services: ['appium'],
-    appium: {
-        command: 'appium',
-        args: ['--use-legacy-sessions'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
     },
+    reporters: ['spec'],
     capabilities: [{
         platformName: 'Android',
         'appium:deviceName': 'emulator-5554',
-        'appium:platformVersion': '11', // Phiên bản Android
+        'appium:platformVersion': '11',
         'appium:app': 'D:\\DamBaoChatLuong\\25-3\\Bambon.apk',
         'appium:appPackage': 'com.huynhbao103.Bambon_Fe',
         'appium:appActivity': 'com.huynhbao103.Bambon_Fe.MainActivity',
+        'appium:automationName': 'UiAutomator2',
         'appium:noReset': true,
         'appium:newCommandTimeout': 300,
-        'appium:automationName': 'UiAutomator2', // Đảm bảo sử dụng UiAutomator2
     }],
     logLevel: 'info',
     bail: 0,
-    baseUrl: 'http://localhost:4723', // Đảm bảo Appium server đang chạy tại đây
     waitforTimeout: 10000,
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000,
-    },
 };
